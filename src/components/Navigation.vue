ebca9842acf725757296d3dfb778fe199fcde878
ducnguyenhuynh <ducnghh@gmail.com>
2024-08-31 00:45:53 +0700

    update the sidebar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -7,6 +7,7 @@ import apartment from './icons/apartment.vue';
 import camera from './icons/camera.vue';
 import users from './icons/users.vue';
 import docs from './icons/docs.vue';
+import setting from './icons/setting.vue'
 const expanded = ref(true)
 
 const menuItems = ref([
@@ -31,15 +32,15 @@ function toggle () {
     </div>
 
     <div v-if="expanded">
-        <div class="h-screen w-48 bg-white rounded-lg items-center justify-center shadow-md border-r-2 border-gradient-br-blue-green-gray-900 border-transparent">
-            <div class="space-y-1 p-4 justify-center items-center grid grid-flow-row">
-                <button @click="toggle" class="transform transition duration-500 ease-in-out hover:scale-125 justify-center px-3 py-2.5 text-center inline-flex items-center me-2 mb-1">
-                    <chevron></chevron>
+        <div class="h-screen flex-row w-44 bg-white rounded-lg items-center justify-center shadow-md border-r-2 border-gradient-br-blue-green-gray-900 border-transparent">
+            <div class="space-y-1 p-4 justify-center items-center grid grid-flow-row pr-6 mx-6 pb-6 border-b-2">
+                <button @click="toggle" class="transform transition duration-500 ease-in-out hover:scale-125 mt-4 mx-2 bnt">
+                    <chevron ></chevron>
                 </button>
-                <img src="@/assets/logo.png" class="w-16 mr-2"/>
+                <img src="@/assets/logo.png" class=" w-28 mb-2"/>
             </div>
             <div class="space-y-6 pt-6 font-sans">
-                <div v-for="item of menuItems" class="w-40">
+                <div v-for="item of menuItems" class="w-36">
                     <button class="ml-4 p-2 transform transition duration-500 ease-in-out hover:scale-110 justify-center text-left font-sans w-full rounded-lg hover:border-y-2 hover:border-gradient-tl-blue-green-gray-900 border-transparent">
                         <div class="grid grid-flow-col text-gray-600 ">
                             <component :is="item.icon"></component>
@@ -48,19 +49,26 @@ function toggle () {
                     </button>
                 </div>
             </div>
-            <div class="space-y-6 pt-6 font-semibold">
-                <div>
-
+            <div class="h-1/3 m-6">
+                <div class="w-full h-full justify-center items-center grid grid-flow-col place-content-end">
+                    <button class="transform hover:rotate-90 hover:scale-110">
+                        <setting></setting>
+                    </button>
                 </div>
             </div>
+            <div class="h-6 m-6">
+                <div class="border-t-2"></div>
+            </div>
         </div>
-        
     </div>
 </template>
 
 <style scoped>
 .bnt {
-    @apply transform transition duration-500 ease-in-out hover:scale-110 justify-center bg-gradient-to-br hover:from-sky-300 hover:to-green-300 focus:ring-2 focus:outline-none font-semibold rounded-lg text-sm px-3 py-2.5 text-center inline-flex items-center me-2 mb-1;    
+    @apply transform transition duration-500 ease-in-out hover:scale-110 justify-center bg-gradient-to-br hover:from-sky-300 hover:to-green-300 focus:ring-2 focus:outline-none font-sans rounded-lg text-sm px-3 py-2.5 text-center inline-flex items-center me-2 mb-1;    
+}
+.rounded-lg {
+    border-radius: 0.7rem;    
 }
 .border-gradient-br-blue-green-gray-900 {
   background: linear-gradient(to right, #ffffff, #ffffff),
